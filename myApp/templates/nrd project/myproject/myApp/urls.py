841ba86c7d437a68ffe532e0977ce3560a580ea5
Rasikaganesan <164046654+Rasikaganesan@users.noreply.github.com>
2025-11-01 14:30:25 +0530

    Delete myApp/templates directory

--- myApp/templates/nrd project/myproject/myApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path("" ,views.index,name="index"),
-    path("post" ,views.details,name="details"),
-    path("clear" ,views.screen,name="screen")
-]
